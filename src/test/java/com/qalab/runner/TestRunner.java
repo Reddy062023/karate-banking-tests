921bd03daccbea4f9abbe7c58de7c18f8458d5d2
Geetu <japendareddy@gmail.com>
2026-04-17 08:24:46 -0400

    Add AllureKarate hook - generate allure-results for banking tests

--- src/test/java/com/qalab/runner/TestRunner.java
+++ src/test/java/com/qalab/runner/TestRunner.java
@@ -1,12 +1,23 @@
 package com.qalab.runner;
 
-import com.intuit.karate.junit5.Karate;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import io.qameta.allure.karate.AllureKarate;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestRunner {
 
-    @Karate.Test
-    Karate testAll() {
-        return Karate.run("classpath:banking")
-                .tags("~@ignore");
+    @Test
+    void testAll() {
+        Results results = Runner
+                .path("classpath:banking")
+                .tags("~@ignore")
+                .hook(new AllureKarate())
+                .outputHtmlReport(true)
+                .outputCucumberJson(true)
+                .parallel(1);
+        assertEquals(0, results.getFailCount(),
+                results.getErrorMessages());
     }
 }
